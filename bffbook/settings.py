c608778b72c083b10c79ae6573f767b2861cf9c8
Naveen <healthapp.innovation@gmail.com>
2020-09-25 21:33:47 +0530

    req updated

--- bffbook/settings.py
+++ bffbook/settings.py
@@ -133,3 +133,7 @@ STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static_cdn", "static_root
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static_cdn", "media_root")
+
+
+# Activate Django-Heroku.
+django_heroku.settings(locals())
